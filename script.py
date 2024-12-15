cc7939e41beff3c76489f273b20c0a510a6c6853
alex-odilon <prof.alexodilon@gmail.com>
2024-12-15 13:53:41 -0300

    test

--- script.py
+++ script.py
@@ -4,12 +4,10 @@ def process_file(file_path):
         print(data)
     return len(data)
 
-
 def divide_numbers(a, b):
     if b == 0:
         return "Error: Division by zero"
     return a / b
-
 
 result = divide_numbers(10, 0)
 print("Result is:", result)

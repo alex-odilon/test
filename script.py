23e20d0ef84e5d904899ca9873a8b303a6b84359
alex-odilon <prof.alexodilon@gmail.com>
2024-12-15 11:24:43 -0300

    first commit

---
+++ script.py
@@ -0,0 +1,18 @@
+import os
+import sys
+
+
+def process_file(file_path):
+    file = open(file_path, "r")
+    data = file.read()
+    print(data)
+    file.close()
+    return len(data)
+
+
+def divide_numbers(a, b):
+    return a / b
+
+
+result = divide_numbers(10, 0)
+print("Result is:", result)

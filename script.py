a0557af353983908ea7bcf53890887728e5ac115
alex-odilon <prof.alexodilon@gmail.com>
2024-12-15 13:57:14 -0300

    test

--- script.py
+++ script.py
@@ -1,4 +1,16 @@
+"""
+Este módulo contém funções para processar arquivos e realizar divisões.
+A função 'process_file' lê um arquivo e retorna seu tamanho, enquanto 'divide_numbers'
+realiza uma divisão entre dois números e trata a divisão por zero.
+"""
+
 def process_file(file_path):
+    """
+    Lê o conteúdo de um arquivo e imprime seu conteúdo.
+
+    :param file_path: Caminho do arquivo a ser processado.
+    :return: O tamanho do conteúdo lido do arquivo.
+    """
     with open(file_path, "r", encoding="utf-8") as file:
         data = file.read()
         print(data)
@@ -6,6 +18,13 @@ def process_file(file_path):
 
 
 def divide_numbers(a, b):
+    """
+    Realiza a divisão de dois números e trata a divisão por zero.
+
+    :param a: O numerador da divisão.
+    :param b: O denominador da divisão.
+    :return: O resultado da divisão ou uma mensagem de erro em caso de divisão por zero.
+    """
     if b == 0:
         return "Error: Division by zero"
     return a / b

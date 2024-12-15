13f05647c84397ac279aaa9ff7f445642eb4566b
alex-odilon <prof.alexodilon@gmail.com>
2024-12-15 12:02:27 -0300

    first commit

--- script2.py
+++ script2.py
@@ -1,4 +1,5 @@
 def count_lines(file_path):
+
     try:
         with open(file_path, 'r') as file:
             lines = file.readlines()
@@ -7,12 +8,16 @@ def count_lines(file_path):
         print(f"Erro ao abrir o arquivo: {e}")
         return None
 
+
 def multiply_numbers(a, b):
+
     return a * b
+
 
 file_path = "example.txt"
 lines_count = count_lines(file_path)
 print(f"O número de linhas em {file_path} é {lines_count}")
+
 
 result = multiply_numbers(3, 7)
 print(f"O resultado da multiplicação é: {result}")
